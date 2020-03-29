0c2821fb1fc63c82aa5d76b7918fbc86db7e5f05
looper-m <mothil.vu@gmail.com>
2020-03-28 20:37:55 -0700

    need errand activity

--- app/src/main/java/com/example/errand/NeedErrandActivity.java
+++ app/src/main/java/com/example/errand/NeedErrandActivity.java
@@ -28,7 +28,7 @@ public class NeedErrandActivity extends Activity {
             }
         });
 
-        findViewById(R.id.see_requests_button).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.see_errands_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(NeedErrandActivity.this, PostedErrandsActivity.class);
